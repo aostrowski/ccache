191d76aac28c03c4626e45fb3f2ff9522097a809
Adrian Ostrowski <aostrowski@habana.ai>
2022-09-22 20:40:14 +0200

    Make clang-tidy tests pass

--- test/suites/tidy.bash
+++ test/suites/tidy.bash
@@ -13,86 +13,80 @@ EOF
 tidy_tests() {
     # -------------------------------------------------------------------------
     TEST "Positive case - no diagnostics"
-    echo dupa $RAW_STDOUT
 
-    tidy_opts_cpp="-checks=*,-modernize-use-trailing-return-type"
-    ccache_tidy_cpp="$CCACHE $REAL_TIDY $tidy_opts_cpp --"
+    tidy_cpp="$REAL_TIDY -checks=*,-modernize-use-trailing-return-type"
+    ccache_tidy_cpp="$CCACHE $tidy_cpp"
 
 
     # Run TIDY without CCache
-    capture_output RAW_STDOUT RAW_STDERR $REAL_TIDY $tidy_opts_cpp test_tidy.cpp
-    echo dupa $RAW_STDOUT
-
-    expect_equal "" "$RAW_STDOUT"
+    capture_output RAW_STDOUT RAW_STDERR $tidy_cpp test_tidy.cpp --
 
     # First TIDY run
-    capture_output FIRST_STDOUT FIRST_STDERR $ccache_tidy_cpp test_tidy.cpp
+    capture_output FIRST_STDOUT FIRST_STDERR $ccache_tidy_cpp test_tidy.cpp --
     expect_stat preprocessed_cache_hit 0
     expect_stat cache_miss 1
     expect_stat files_in_cache 1
-    expect_equal "" "$FIRST_STDOUT"
+    expect_equal "$RAW_STDOUT" "$FIRST_STDOUT"
     expect_equal "$RAW_STDERR" "$FIRST_STDERR"
 
     # Second TIDY run
-    capture_output SECOND_STDOUT SECOND_STDERR $ccache_tidy_cpp test_tidy.cpp
+    capture_output SECOND_STDOUT SECOND_STDERR $ccache_tidy_cpp test_tidy.cpp --
     expect_stat preprocessed_cache_hit 1
     expect_stat cache_miss 1
     expect_stat files_in_cache 1
-    expect_equal "" "$SECOND_STDOUT"
+    expect_equal "$RAW_STDOUT" "$SECOND_STDOUT"
     expect_equal "$RAW_STDERR" "$SECOND_STDERR"
 
     # -------------------------------------------------------------------------
     TEST "Negative case - some diagnostics"
 
-    tidy_opts_cpp="-checks=*"
-    ccache_tidy_cpp="$CCACHE $REAL_TIDY $tidy_opts_cpp --"
+    tidy_cpp="$REAL_TIDY -checks=*"
+    ccache_tidy_cpp="$CCACHE $tidy_cpp"
 
 
     # Run TIDY without CCache
-    capture_output RAW_STDOUT RAW_STDERR $REAL_TIDY $tidy_opts_cpp test_tidy.cpp
-    expect_equal "" "$RAW_STDOUT"
+    capture_output RAW_STDOUT RAW_STDERR $tidy_cpp test_tidy.cpp --
 
     # First TIDY run
-    capture_output FIRST_STDOUT FIRST_STDERR $ccache_tidy_cpp test_tidy.cpp
+    capture_output FIRST_STDOUT FIRST_STDERR $ccache_tidy_cpp test_tidy.cpp --
     expect_stat preprocessed_cache_hit 0
     expect_stat cache_miss 1
     expect_stat files_in_cache 1
-    expect_equal "" "$FIRST_STDOUT"
+    expect_equal "$RAW_STDOUT" "$FIRST_STDOUT"
     expect_equal "$RAW_STDERR" "$FIRST_STDERR"
 
     # Second TIDY run
-    capture_output SECOND_STDOUT SECOND_STDERR $ccache_tidy_cpp test_tidy.cpp
+    capture_output SECOND_STDOUT SECOND_STDERR $ccache_tidy_cpp test_tidy.cpp --
     expect_stat preprocessed_cache_hit 1
     expect_stat cache_miss 1
     expect_stat files_in_cache 1
-    expect_equal "" "$SECOND_STDOUT"
+    expect_equal "$RAW_STDOUT" "$SECOND_STDOUT"
     expect_equal "$RAW_STDERR" "$SECOND_STDERR"
 
     # -------------------------------------------------------------------------
     TEST "Compilation error"
 
-    tidy_opts_cpp="-checks=* -warnings-as-errors=*"
-    ccache_tidy_cpp="$CCACHE $REAL_TIDY $tidy_opts_cpp --"
+    tidy_cpp="$REAL_TIDY -checks=* -warnings-as-errors=*"
+    ccache_tidy_cpp="$CCACHE $tidy_cpp"
 
 
     # Run TIDY without CCache
-    capture_output RAW_STDOUT RAW_STDERR $REAL_TIDY $tidy_opts_cpp test_tidy.cpp
-    expect_equal "" "$RAW_STDOUT"
+    capture_output RAW_STDOUT RAW_STDERR $tidy_cpp test_tidy.cpp --
 
     # First TIDY run
-    capture_output FIRST_STDOUT FIRST_STDERR $ccache_tidy_cpp test_tidy.cpp
+    capture_output FIRST_STDOUT FIRST_STDERR $ccache_tidy_cpp test_tidy.cpp --
     expect_stat preprocessed_cache_hit 0
     expect_stat cache_miss 1
     expect_stat files_in_cache 1
-    expect_equal "" "$FIRST_STDOUT"
+    expect_equal "$RAW_STDOUT" "$FIRST_STDOUT"
     expect_equal "$RAW_STDERR" "$FIRST_STDERR"
 
     # Second TIDY run
-    capture_output SECOND_STDOUT SECOND_STDERR $ccache_tidy_cpp test_tidy.cpp
+    capture_output SECOND_STDOUT SECOND_STDERR $ccache_tidy_cpp test_tidy.cpp --
     expect_stat preprocessed_cache_hit 1
     expect_stat cache_miss 1
     expect_stat files_in_cache 1
-    expect_equal "" "$SECOND_STDOUT"
+    expect_equal "$RAW_STDOUT" "$SECOND_STDOUT"
     expect_equal "$RAW_STDERR" "$SECOND_STDERR"
 }
 
